ea70f75a9046a9304cce91ddf893915e4ef1d0c3
Jimy Huacho Dolores <jimmyhd_19@hotmail.com>
2021-03-29 17:16:40 -0500

    maqueta login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 @NgModule({
-	declarations: [AppComponent],
-	imports: [BrowserModule, BrowserAnimationsModule, MatButtonModule],
+	declarations: [AppComponent, LoginComponent],
+	imports: [
+		BrowserModule,
+		BrowserAnimationsModule,
+		MatButtonModule,
+		MatFormFieldModule,
+		MatIconModule,
+		MatInputModule,
+		FormsModule
+	],
 	providers: [],
 	bootstrap: [AppComponent]
 })
